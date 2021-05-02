8a361c3795521a8200ef005da87086e61adb4db0
layersony <sammaingi5@gmail.com>
2021-05-02 23:17:20 +0300

    logic and coreectness of question inserted

--- js/script.js
+++ js/script.js
@@ -11,11 +11,12 @@ Student.prototype.fullname = function(){
   return this.firstname +" "+ this.surname
 }
 
-let TestScore = function(){
+let TestScore = function(singlearrays){
   total = 0
-  for (var i=0; i<arguments.length; i++){
-    total += arguments[i]
-  }
+  singlearrays.forEach(function(singlearray){
+    total += singlearray
+  })
+  
   return total
 }
 
@@ -35,5 +36,40 @@ $(document).ready(function(){
     if(fName && sName){
       alert(newStudent.fullname())
     }
+  });
+
+  $('#formquiz').submit(function(event){
+    event.preventDefault()
+    let quiz1 = $('input:radio[name=1]:checked').val();
+    let quiz2 = $('input:radio[name=2]:checked').val();
+    let quiz3 = $('input:radio[name=3]:checked').val();
+
+    examScore=[];
+
+    if (quiz1 === 'HTML'){
+      examScore.push(2);
+      $('#tick1').prepend('<i class="far fa-check"></i>')
+    }else{
+      examScore.push(0);
+      $('#tick1').prepend('<i class="far fa-times"></i>')
+    }
+
+    if (quiz2 === 'js'){
+      examScore.push(3);
+      $('#tick2').prepend('<i class="far fa-check"></i>')
+    }else{
+      examScore.push(0);
+      $('#tick2').prepend('<i class="far fa-times"></i>')
+    }
+
+    if (quiz3 === 'yes'){
+      examScore.push(3);
+      $('#tick3').prepend('<i class="far fa-check"></i>')
+    }else{
+      examScore.push(0);
+      $('#tick3').prepend('<i class="far fa-times"></i>')
+    }
+
+    TestScore(examScore)
   });
 });
